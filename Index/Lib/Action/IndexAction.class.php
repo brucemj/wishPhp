85a480c000fc19b131b08de29d27e822a05ca750
mojun <mojun@konka.com>
2014-09-10 11:38:34 +0800

    replace Public conf ; show wish index template

--- Index/Lib/Action/IndexAction.class.php
+++ Index/Lib/Action/IndexAction.class.php
@@ -2,8 +2,8 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
-    	echo U('wish');
-		p(__SERVER__);
-	//$this -> display();
+    	//echo U('wish');
+		//p(__PUBLIC__);
+		$this -> display();
     }
 }
