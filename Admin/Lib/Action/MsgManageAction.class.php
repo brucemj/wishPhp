4ab87dab0b2f1689e61ebcc7ed2c64dfb73a7221
mojun <mojun@konka.com>
2014-09-18 10:01:53 +0800

    show msg is ok

--- Admin/Lib/Action/MsgManageAction.class.php
+++ Admin/Lib/Action/MsgManageAction.class.php
@@ -1,9 +1,26 @@
 <?php
 	Class MsgManageAction extends CommonAction {
+		//帖子查看
 		public function index(){
+			$count = M('msg')->count();
+			 
+			import('ORG/Util/Page');
+			$page = new Page($count ,10);
+			
+			$limit = $page->firstRow . ',' . $page->listRows ;
+			
+			$this->page = $page->show();			
+			$this->wish = M('msg')->limit($limit)->select();
 			$this->display();
 		}
 		
-		
+		//帖子删除
+		public function delete() {
+			if (M(msg)->delete(I('get.id')) ){
+				$this->success('删除成功', U('Admin/MsgManage/index','',''));
+			} else{
+				$this->error('删除失败');
+			}
+		}
 	}
 ?>
